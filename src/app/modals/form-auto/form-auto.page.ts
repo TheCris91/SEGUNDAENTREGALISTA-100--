7dc86efa4b9d85de21b95e768a190717b09cd2b7
TheCris91 <arredondocristopher43@gmail.com>
2023-11-28 00:22:57 -0300

    listo todo

--- src/app/modals/form-auto/form-auto.page.ts
+++ src/app/modals/form-auto/form-auto.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Preferences } from '@capacitor/preferences';
 import { HelperService } from 'src/app/services/helper.service';
 import { StorageService } from 'src/app/services/storage.service';
-import { InformacionPage } from '../informacion/informacion.page';
-import { ImagenService } from 'src/app/services/imagen.service';
 
 @Component({
   selector: 'app-form-auto',
@@ -11,36 +9,15 @@ import { ImagenService } from 'src/app/services/imagen.service';
   styleUrls: ['./form-auto.page.scss'],
 })
 export class FormAutoPage implements OnInit {
-  auto: any[] = [];
+  autos: any[] = [];
   imagenSeleccionadaURL: string = '';
 
   constructor(
-    private storageService: StorageService,
+    //private storageService: StorageService,
     private helperService: HelperService,
-    private imagenService: ImagenService,
   ) {}
 
   ngOnInit() {}
-
-  /*seleccionarImagen(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const imagen = input.files[0];
-      const imagenURL = URL.createObjectURL(imagen);
-      this.imagenService.setImagenSeleccionadaURL(imagenURL);
-    }
-  }
-  imagenSeleccionada: boolean = false;
-
-  seleccionarImagen(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const imagen = input.files[0];
-      const imagenURL = URL.createObjectURL(imagen);
-      this.imagenSeleccionadaURL = imagenURL;
-      this.imagenSeleccionada = true;
-    }
-  }*/
 
   guardarAuto() {
     const patenteInput = document.getElementById('Patente') as HTMLIonInputElement;
@@ -48,8 +25,7 @@ export class FormAutoPage implements OnInit {
     const modeloInput = document.getElementById('Modelo') as HTMLIonInputElement;
     const marcaInput = document.getElementById('Marca') as HTMLIonInputElement;
     const colorInput = document.getElementById('Color') as HTMLIonInputElement;
-    
-  
+
     const patente = patenteInput.value;
     const pato = patoInput.value;
     const modelo = modeloInput.value;
@@ -60,35 +36,37 @@ export class FormAutoPage implements OnInit {
       this.helperService.showAlert('Por favor, complete todos los campos.', 'Error');
       return;
     }
-  
+
     if (typeof patente !== 'string' || patente.length !== 6) {
       this.helperService.showAlert('La patente debe tener exactamente 6 caracteres.', 'Error');
       return;
     }
-  
+
+    // Verificar si la patente ya existe
+    if (this.autos.some((auto) => auto.patente === patente)) {
+      this.helperService.showAlert('La patente ya existe.', 'Error');
+      return;
+    }
+
     if (typeof pato !== 'string' || pato.length !== 4 || isNaN(Number(pato))) {
       this.helperService.showAlert('El año del automóvil debe contener exactamente 4 dígitos numéricos.', 'Error');
       return;
     }
-  
+
     if (typeof modelo !== 'string' || modelo.length < 3 || modelo.length > 40) {
       this.helperService.showAlert('El modelo debe tener entre 3 y 40 caracteres.', 'Error');
       return;
     }
-  
+
     if (typeof marca !== 'string' || marca.length < 3 || marca.length > 40) {
       this.helperService.showAlert('La marca debe tener entre 3 y 40 caracteres.', 'Error');
       return;
     }
-  
+
     if (typeof color !== 'string' || color.length < 3 || color.length > 40) {
       this.helperService.showAlert('El color debe tener entre 3 y 40 caracteres.', 'Error');
       return;
     }
-    /*if (!this.imagenSeleccionada) {
-      this.helperService.showAlert('Por favor, suba una imagen del automóvil.', 'Error');
-      return;
-    }*/
 
     const auto = {
       patente: patente,
@@ -96,23 +74,20 @@ export class FormAutoPage implements OnInit {
       marca: marca,
       pato: pato,
       color: color,
-      /*imagenURL: this.imagenSeleccionadaURL,*/
-      
     };
 
-    // Guardar el automóvil en el almacenamiento
-    localStorage.setItem('auto', JSON.stringify(auto));
-    
-    this.storageService.guardarAuto(auto);
+    // Agregar el nuevo auto a la lista
+    this.autos.push(auto);
+
+    // Guardar la lista actualizada en el almacenamiento
+    Preferences.set({ key: 'autos', value: JSON.stringify(this.autos) });
+
     this.helperService.showAlert('Datos agregados con éxito.', 'Éxito');
-  
-    // Limpiar los campos de entrada
+
     patenteInput.value = '';
     patoInput.value = '';
     modeloInput.value = '';
     marcaInput.value = '';
     colorInput.value = '';
-    /*this.imagenSeleccionadaURL = '';*/
   }
-  
 }
